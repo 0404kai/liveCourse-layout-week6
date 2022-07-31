3345d6e2352e15364f6b1e2f462995557cdd71fd
Darren_Hsu <bthd0404@gmail.com>
2022-08-01 01:17:43 +0800

    Update 2022-07-31T17:17:40.048Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,58 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
+//選單
+$('#burger').on('click', function () {
+  $('#menu').slideToggle();
+}); //輪播
+
+var mySwiper = new Swiper('.swiper-course', {
+  autoplay: {
+    disableOnInteraction: false,
+    delay: 0
+  },
+  speed: 1500,
+  loop: true,
+  freeMode: true,
+  //自由模式
+  slidesPerView: 3,
+  //多欄
+  breakpoints: {
+    768: {
+      slidesPerView: 6
+    },
+    992: {
+      slidesPerView: 9
+    }
+  }
+});
+var mySwiper = new Swiper('.swiper-feedback', {
+  autoplay: {
+    disableOnInteraction: false
+  },
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination'
+  }
+}); //日期
+
+var date = document.querySelector('#date');
+var datepicker = new Datepicker(date, {
+  language: 'zh-TW',
+  orientation: 'bottom'
+});
+var effectiveDate = document.querySelector('#effective-date');
+var datepicker = new Datepicker(effectiveDate, {
+  language: 'zh-TW',
+  orientation: 'bottom',
+  format: 'MM-yy'
+}); //結帳頁切換
+
+$('#checkout').on('click', function () {
+  $('#apply').fadeOut();
+  $('#creditCard').fadeIn();
+});
+$('#next').on('click', function () {
+  $('#creditCard').fadeOut();
+  $('#success').fadeIn();
 });
 //# sourceMappingURL=all.js.map
